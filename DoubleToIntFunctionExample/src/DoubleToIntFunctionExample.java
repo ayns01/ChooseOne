ec2d37445759c7f58f440e777ec192cf1cb7df56
ayns <peenats1203@gmail.com>
2018-07-23 06:24:45 +0900

    created DoubleToIntFunction

---
+++ DoubleToIntFunctionExample/src/DoubleToIntFunctionExample.java
@@ -0,0 +1,10 @@
+import java.util.function.DoubleToIntFunction;
+
+public class DoubleToIntFunctionExample {
+
+    public static void main(String[] args) {
+        DoubleToIntFunction df = (x) -> { return (int)x + 2;};
+
+        System.out.println(df.applyAsInt(3.14));
+    }
+}
