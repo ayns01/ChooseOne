ddc436f45741da0eb88b75d102f7f3c6ea0d210e
ayns <peenats1203@gmail.com>
2018-07-23 05:52:54 +0900

    created DoubleFunction

---
+++ DoubleFunction/src/DoubleFunctionExample.java
@@ -0,0 +1,10 @@
+import java.util.function.DoubleFunction;
+
+public class DoubleFunctionExample {
+
+    public static void main(String[] args) {
+        DoubleFunction<String> df = (d) -> d + " is now a string";
+
+        System.out.println(df.apply(0.5));
+    }
+}
