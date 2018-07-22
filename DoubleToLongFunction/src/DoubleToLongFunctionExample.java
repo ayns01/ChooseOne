93bf0f80a09a2e7b00918cb0d0d89d6cf5e3f600
ayns <peenats1203@gmail.com>
2018-07-23 06:29:42 +0900

    created DoubleToLongFunction

---
+++ DoubleToLongFunction/src/DoubleToLongFunctionExample.java
@@ -0,0 +1,10 @@
+import java.util.function.DoubleToLongFunction;
+
+public class DoubleToLongFunctionExample {
+
+    public static void main(String[] args) {
+        DoubleToLongFunction dl = (x) -> { return Long.MAX_VALUE - (long)x;};
+
+        System.out.println(dl.applyAsLong(3.14));
+    }
+}
